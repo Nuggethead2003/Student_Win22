1e925c29b46fc1a9c8f0512b6e76e47545e1b098
amir <amir@localhost>
2022-01-25 09:22:22 -0500

    Added the whole thing lmaoooooo

---
+++ TestApp/src/Student.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author amir
+ */
+public class Student {
+    private String studentId;
+    private String studentName;
+    private String address;
+
+    
+    
+    public Student(String studentId, String studentName){
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+    
+    
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    
+}

06522fec8a551cbf0473eb88752682a283b734b2
Nuggethead2003 <amirrezaghiassian7@gmail.com>
2022-01-25 10:21:03 -0500

    removed println

--- TestApp/src/testapp/TestApp.java
+++ TestApp/src/testapp/TestApp.java
@@ -14,7 +14,7 @@ public class TestApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hello world");
+        
     }
     
 }
